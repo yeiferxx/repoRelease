8562bcf3eb5254189ec3ee0b7d17aadfac989db6
Yeison Delgado <delgadoyeisonfer@gmail.com>
2024-12-04 16:25:38 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repoRelease",                             # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Yeison Delgado",                         # Tu nombre
+    author_email="delgadoyeisonfer@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/yeiferxx/repoRelease",     # URL del proyecto
+)
